d99150c85ce7f516b433f577e5cdd48306a821e6
ludovicChevallier <45288555+ludovicChevallier@users.noreply.github.com>
2022-01-10 17:41:55 +0100

    second KNN

--- recommendation_system/KNN_recommendation_2.py
+++ recommendation_system/KNN_recommendation_2.py
@@ -19,26 +19,40 @@ def load_json():
     list_item=[]
     list_user=[]
     test_cases=[]
+    id="PC0"
     for j in patients_df.index:
         if(patients_df["conditions"][j][0]["cured"]==None):
             test_cases.append(patients_df.iloc[j])
+        if(j<250):
+            for trial in patients_df["trials"][j]:
+                    if(trial["condition"]!=id):
+                        list_user.append(patients_df["id"][j])
+                        list_rating.append(int(trial["sucessful"].split("%")[0]))                
+                        list_item.append(trial["therapy"])
+    test_dict={"user": list_user,"item": list_item,"rating": list_rating}
+    list_rating=[]
+    list_item=[]
+    list_user=[]
     for j in patients_df.index:       
         for trial in patients_df["trials"][j]:
-            list_user.append(patients_df["id"][j])
-            list_rating.append(int(trial["sucessful"].split("%")[0]))
-            list_item.append(trial["therapy"])
-    ratings_dict = {"user": list_user,"item": list_item,"rating": list_rating,}
-    return ratings_dict,test_cases
+            if j<250 and trial["condition"]==id or j>250 :
+                list_user.append(patients_df["id"][j])
+                list_rating.append(int(trial["sucessful"].split("%")[0]))
+            #In this case we only want to take the therapy link to the condition we want to cure
+            
+                list_item.append(trial["therapy"])
+    ratings_dict = {"user": list_user,"item": list_item,"rating": list_rating}
+    return ratings_dict,test_cases,test_dict
 
 def main():
-    ratings_dict,test_cases=load_json()
+    ratings_dict,test_cases,test_dict=load_json()
     df = pd.DataFrame(ratings_dict)
     reader = Reader(rating_scale=(0, 100))
     data = Dataset.load_from_df(df[["user", "item", "rating"]], reader)
     sim_options = {
         "name": "cosine", #use cosine similarity
         "min_support": 3,
-        "user_based": False,  # Compute  similarities between items
+        "user_based": False,  # Compute  similarities between user
     }
     trainingSet = data.build_full_trainset()
     algo = KNNWithMeans(sim_options=sim_options)
@@ -57,7 +71,10 @@ def main():
                     max_value=val
                     best_thera=elem
         print("the best therapy for the user "+test["name"]+" is the therapy: "+best_thera+" with a value of "+str(max_value))
-    trainset, testset = train_test_split(data, test_size=0.25)
-    predictions = algo.fit(trainset).test(testset)
-    accuracy.rmse(predictions)
+    df = pd.DataFrame(test_dict)
+    reader = Reader(rating_scale=(0, 100))
+    data = Dataset.load_from_df(df[["user", "item", "rating"]], reader)
+    testset = [data.df.loc[i].to_list() for i in range(len(data.df))]
+    prediction=algo.test(testset)
+    accuracy.rmse(prediction)
 main()
